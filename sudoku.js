61ce192f01c8aecbab47c498706f7846d50a0d92
Tobias Kusnaman <tobiaskusnaman@Tobiass-MacBook-Pro.local>
2017-11-17 16:17:06 +0700

    Release 0 done

--- sudoku.js
+++ sudoku.js
@@ -1,13 +1,29 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.board_string = board_string;
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    //bikin board
+  let papan = [];
+    for (var i = 0; i <= this.board_string.length-1; i+= 9) {
+      papan.push(this.board_string.substr(i,9).split(''));
+    }
+    return papan;
+  }
+
+  cekRow(){
+    //cek satu baris dulu ada 1-9 gak?
+    //perulangan tiap i
+    
+  }
 }
+
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
@@ -16,7 +32,7 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[0]
 
-var game = new Sudoku(board_string)
+var game = new Sudoku('105802000090076405200400819019007306762083090000061050007600030430020501600308900');
 
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
