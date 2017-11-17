7f633f6f7b6a24abcd7b7395f73c5ff7bd5d5635
Ayunda <ayundahamzah@gmail.com>
2017-11-17 17:08:43 +0700

    release 3

--- sudoku.js
+++ sudoku.js
@@ -3,10 +3,9 @@
 class Sudoku {
   constructor(board_string) {
     this.SudokuBoard=[]
-  }
-
-  solve() {
-
+    this.arrZero=[]
+    // this.random= Math.ceil(Math.random()*9)
+    this.angka='123456789'
   }
 
   board() {
@@ -18,8 +17,72 @@ class Sudoku {
         counter++
       }
     }
-    console.log(this.SudokuBoard);
+    return this.SudokuBoard;
   }
+
+  //mencari koordinat 0 (yang nanti valuenya harus di ganti)
+  findZero (){
+    this.board();
+    // console.log(this.board());
+    for (let i = 0; i < this.SudokuBoard.length; i++) {
+      for (let j = 0; j < this.SudokuBoard.length; j++) {
+        if (this.SudokuBoard[i][j] == 0) {
+          let zeroCoor = i+''+j;
+          this.arrZero.push(zeroCoor);
+        }
+      }
+    }
+    console.log(this.arrZero)
+    return this;
+  }
+//===============================CHECKER=========================================
+  checkRow(row,tebakAngka){
+    this.findZero();
+    let getRow = row[1];
+    // console.log(this.arrZero);
+    for(let i=0;i<9;i++){
+          if(tebakAngka!==this.SudokuBoard[i][getRow]){
+            return true
+          }else{
+            false
+          }
+    }
+  }
+
+  checkColumn(column,tebakAngka){
+    this.findZero();
+    let getCol = column[0];
+    for(let i = 0; i < 9;i++){
+      if (tebakAngka!==this.SudokuBoard[getCol][i]){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  }
+
+  checkArea(baris,tebakAngka){
+    let getCol = Math.floor(baris[1] / 3) * 3;
+    let getRow = Math.floor(baris[0] / 3) * 3;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++){
+          if (this.SudokuBoard[getRow+i][getCol+j] == tebakAngka) {
+            return true;
+          }
+        }
+      }
+    return false;
+  }
+//=============================END OF CHECKER===================================
+
+
+
+
+
+
+
+
+
 
 }
 
@@ -33,6 +96,9 @@ let board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 let game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+// game.solve()
 
-console.log(game.board())
+// console.log(game.board())
+// console.log(game.findZero())
+// console.log(game.checkColumn());
+console.log(game.checkRow());
