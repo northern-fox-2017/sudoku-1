281ecb047d1b5a9ad8cac934dcc534230e1e4d9f
rezaalfarabi <anangreza18@gmail.com>
2017-11-17 18:58:21 +0700

    sudoku check kolom, row dan area

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,69 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.number     = board_string
+    this.boards     = [];
+    this.setZero       = [];
+  }
 
-  solve() {}
+  solve() {
 
-  // Returns a string representing the current state of the board
-  board() {}
+  }
+
+  board() {
+    let count = 0
+    for(let i = 0 ; i < 9 ; i++) {
+      this.boards.push([]);
+        for(let j = 0 ; j < 9 ; j++) {
+          this.boards[i].push(this.number[count])
+          count++;
+        }
+    }
+    console.log(this.boards);
+  }
+
+  getZero() { // fungsi untuk menentukan setZero baris dan kolom
+    for(let i = 0 ; i < this.boards.length ; i++) {
+      for(let j = 0 ; j < this.boards[i].length ; j++) {
+        if(this.boards[i][j] === '0') {
+          this.setZero.push([i,j]);
+        }
+      }
+    }
+    return this.setZero;
+  }
+
+  checkRow(baris, angka) {
+    for(let i = 0 ; i < 9 ; i++) {
+      if(this.boards[baris][i] == angka) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkKolom(kolom, angka) {
+    for(let i = 0 ; i < 9 ; i++) {
+      if(this.boards[i][kolom] == angka) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkArea3x3(baris, tebak) {
+  let getKolom = Math.floor(baris[1] / 3) * 3;
+  let getBaris = Math.floor(baris[0] / 3) * 3;
+      for (let k = 0; k < 3; k++) {
+        for (let l = 0; l < 3; l++){
+          if (this.arrBoard[getBaris+k][getKolom+l] == tebak) {
+            return true;
+          }
+        }
+      }
+  return false;
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -18,7 +75,11 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 
 var game = new Sudoku(board_string)
 
-// Remember: this will just fill out what it can and not "guess"
-game.solve()
+game.board();
+console.log(game.checkRow(2,3));
+console.log(game.checkKolom(1,3))
 
-console.log(game.board())
+// Remember: this will just fill out what it can and not "guess"
+// game.solve()
+
+// console.log(game.board())
