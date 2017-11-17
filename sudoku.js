b02a169f2d572c2f3527d3148aaa105f5b4db3d8
juliusade <juliusade15@gmail.com>
2017-11-17 20:04:07 +0700

    release3 cek area

--- sudoku.js
+++ sudoku.js
@@ -14,18 +14,18 @@ class Sudoku {
     for(let i=0; i<9; i++) {
       this.arr.push([]);
       for(let j=0; j<9; j++) {
-        this.arr[i].push(this.board_string[counter++])
+        this.arr[i].push(this.board_string[counter++]);
       }
     }
     // console.log(this.arr);
-    return this.arr
+    return this.arr;
   }
 
 
   checkHorizontal(baris, angka) {
     for(let i=0; i<9; i++) {
       if(this.arr[i][baris] == angka) {
-      return false
+      return false;
       }
     }
     return true;
@@ -41,7 +41,41 @@ class Sudoku {
     return true;
   }
 
+
+  checkArea(baris, kolom, angka) {
+    //bikin kondisi cek baris kolom 3x3
+    var startBaris = 0;
+    if(baris < 3) {
+      startBaris = 0;
+    } else if(baris < 6) {
+      startBaris = 3;
+    } else if (baris < 9) {
+      startBaris = 6;
+    }
+
+    var startKolom = 0;
+    if(kolom < 3) {
+      startKolom = 0;
+    } else if (kolom < 6) {
+      startKolom = 3;
+    } else if (kolom < 9) {
+      startKolom = 6;
+    }
+
+    for(let i=startBaris; i<3; i++) {
+      for(let j=startKolom; j<3; j++) {
+        if(this.arr[i][j] == angka) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
 }
+
+
+
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
@@ -59,6 +93,8 @@ console.log(game.checkHorizontal(6,5));
 console.log(game.checkHorizontal(3,5));
 console.log(game.checkVertical(1,1));
 console.log(game.checkVertical(1,5));
+console.log(game.checkArea(0,0,1));
+console.log(game.checkArea(2,1,8));
 
 
 // console.log(checker());
