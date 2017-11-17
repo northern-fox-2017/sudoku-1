f08f47ed475473643aa3f0319fc127e3a838686a
ariefmanda <arief.manda57@gmail.com>
2017-11-17 17:12:30 +0700

    Release 0

--- sudoku.js
+++ sudoku.js
@@ -1,14 +1,44 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.papan=board_string.split('').map(Number);
+    this.baris=this.board()
+    this.kolom=[]
+    this.ragion=[]
+  }
 
-  solve() {}
+  solve() {
+  
+  }
 
   // Returns a string representing the current state of the board
-  board() {}
-}
+  board() {
+    let result=[]
+    for (let i = 0; i < this.papan.length; i+=9) {
+      let row=[]
+      for (let j = i; j < (i+9); j++) {
+        row.push(this.papan[j])
+      }
+      result.push(row)
+    }
+    return result
+  }
+  get_kolom(){
+    let result=[]
+    for (let i = 0; i < 9; i++) {
+      let colom=[]
+      for (let j = i; j <= (i+72); j+=9) {
+        colom.push(this.papan[j])
+      }
+      result.push(colom)
+    }
+    return result
+  }
+  get_region(){
 
+  }
+}
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
 var fs = require('fs')
@@ -20,5 +50,8 @@ var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
+// console.log(game.get_kolom())
 
+// console.log(typeof  game.papan)
 console.log(game.board())
+console.log(game.get_kolom())
