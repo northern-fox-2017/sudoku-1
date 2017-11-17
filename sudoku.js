4de11e0aad310a6341b1449cf5f964ff18c06377
ariefmanda <arief.manda57@gmail.com>
2017-11-17 19:01:00 +0700

    Check row & colomn

--- sudoku.js
+++ sudoku.js
@@ -4,14 +4,41 @@ class Sudoku {
   constructor(board_string) {
     this.papan=board_string.split('').map(Number);
     this.baris=this.board()
-    this.kolom=[]
+    this.kolom=this.get_kolom();
     this.ragion=[]
   }
 
   solve() {
-  
-  }
 
+    console.log(this.checkRow(1,this.baris[0])) //Inputan Check
+    console.log(this.checkColom(1,this.kolom[1])) 
+
+  }
+  
+  checkRow(input,cek){
+    for (let j = 0; j < cek.length; j++) {
+        if(cek[j]===input){
+          return false
+        }
+      }
+    return true;
+  }
+  checkColom(input,cek){
+    for (let j = 0; j < cek.length; j++) {
+      if(cek[j]===input){
+        return false
+      }
+    }
+  return true;
+  }
+  checkBox(input,cek){
+    for (let j = 0; j < cek.length; j++) {
+      if(cek[j]===input){
+        return false
+      }
+    }
+  return true;
+  }
   // Returns a string representing the current state of the board
   board() {
     let result=[]
@@ -36,7 +63,25 @@ class Sudoku {
     return result
   }
   get_region(){
+    let result=[]
+    for (let i = 0; i < 9; i++) {
+      let region=[]
+      let counter=3;
+      for (let j = i*counter; j <= (i*counter)+2; j++) {
+        
+      }
+      let counter=3;
+      for (let j = i; j <=11 ; j++) {
+        
+      }
+      let counter=3;
+      for (let j = 18; j <=20 ; j++) {
+        
+      }
 
+      result.push(region)
+    }
+    return result
   }
 }
 // The file has newlines at the end of each line,
@@ -50,8 +95,8 @@ var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
-// console.log(game.get_kolom())
+
+
 
 // console.log(typeof  game.papan)
 console.log(game.board())
-console.log(game.get_kolom())
