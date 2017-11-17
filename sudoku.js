9a2ea8f66852f194d32acf301850ab3389572caf
Zaki Elyasa Djauhari <zakielyasadj@gmail.com>
2017-11-17 17:01:36 +0700

    baru sampai board dan isinya

--- sudoku.js
+++ sudoku.js
@@ -2,11 +2,30 @@
 
 class Sudoku {
   constructor(board_string) {}
+  
+       
+  
+  solve() {
+	var tampung = '';
+  }
 
-  solve() {}
-
+  
   // Returns a string representing the current state of the board
-  board() {}
+  
+  board() {
+  var arr = [];
+  var baris = 9;
+  var kolom = 9;
+  var counter = 0;
+  for(var i = 0; i < baris; i++){
+	  arr.push([])
+	  for(var j = 0; j < kolom; j++){
+		  arr[i].push(board_string[counter++])
+		  //counter++
+	  }
+  }
+  return arr
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -16,7 +35,7 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[0]
 
-var game = new Sudoku(board_string)
+var game = new Sudoku('619030040270061008000047621486302079000014580031009060005720806320106057160400030')
 
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
