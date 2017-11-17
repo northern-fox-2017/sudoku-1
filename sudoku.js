faec092366132f48503f2ddf89f4a5c74fa17404
Tobias Kusnaman <tobiaskusnaman@Tobiass-MacBook-Pro.local>
2017-11-17 18:50:27 +0700

    cek kolom, cek row

--- sudoku.js
+++ sudoku.js
@@ -3,27 +3,43 @@
 class Sudoku {
   constructor(board_string) {
     this.board_string = board_string;
+    this.papan = [];
+    this.kamus = '123456789';
+    this.angka = true;
   }
 
-  solve() {}
+  solve() {
+
+  }
 
   // Returns a string representing the current state of the board
   board() {
-    //bikin board
-  let papan = [];
     for (var i = 0; i <= this.board_string.length-1; i+= 9) {
-      papan.push(this.board_string.substr(i,9).split(''));
+      this.papan.push(this.board_string.substr(i,9).split(''));
     }
-    return papan;
+    console.log(this.papan);
   }
 
-  cekRow(){
-    //cek satu baris dulu ada 1-9 gak?
-    //perulangan tiap i
-    
+  checkRow(row,input){
+    for (var k = 0; k < 9; k++) {
+      if(this.papan[row][k] == input){
+        this.angka = false;
+      }
+    }
+    return this.angka;
   }
+
+  checkCol(col,input){
+    for (var m = 0; m < 9; m++) {
+      if(this.papan[m][col] == input){
+        this.angka = false;
+      }
+    }
+    return this.angka;
+  }
+
+
 }
-
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
@@ -36,5 +52,7 @@ var game = new Sudoku('105802000090076405200400819019007306762083090000061050007
 
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
-
-console.log(game.board())
+console.log(game.board());
+// console.log(game.checkRow(0,7));
+console.log(game.checkCol(0,3));
+// console.log(game.board().cekRow());
