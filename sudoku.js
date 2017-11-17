c34b6106e7f5020ebe335cac91a7e6a03d50ff8c
juliusade <juliusade15@gmail.com>
2017-11-17 16:49:01 +0700

    release0

--- sudoku.js
+++ sudoku.js
@@ -1,24 +1,38 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.board_string = board_string;
+    this.arr = [];
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let counter = 0;
+    for(let i=0; i<9; i++) {
+      // var baru = this.board_string.split('')
+      this.arr.push([]);
+      for(let j=0; j<9; j++) {
+        this.arr[i].push(this.board_string[counter++])
+      }
+    }
+    // console.log(this.board_string[0]);
+    return this.arr
+  }
+
 }
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
-var fs = require('fs')
+var fs = require('fs');
 var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[0]
 
 var game = new Sudoku(board_string)
-
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+// game.solve()
 
 console.log(game.board())
