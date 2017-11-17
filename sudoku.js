df64c85ce7d5a80c53c16ad7c477f5ff2555225b
eksant <eksoct@yahoo.com>
2017-11-17 17:11:30 +0700

    Update Release 0

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,23 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.board_string = board_string
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let arr = [], col = 9, row = 9, idx = 0
+    for (let i = 0; i < row; i++) {
+      arr.push([])
+      for (let j = 0; j < col; j++) {
+        arr[i].push(Number(this.board_string[idx++]))
+      }
+    }
+    return arr;
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -19,6 +30,6 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+// game.solve()
 
 console.log(game.board())
