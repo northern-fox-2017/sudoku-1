f18ccbb2fd222bc9a63c1c9ec863b0c1386d1062
aldiws <syldie.aldiwijaya@gmail.com>
2017-11-17 19:06:11 +0700

    sudoku release 0,1,2

--- sudoku.js
+++ sudoku.js
@@ -1,24 +1,61 @@
-"use strict"
-
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.board_string = board_string
+    this.result = []
+  }
 
-  solve() {}
+  board() {
+    var arrayString = board_string.toString().split('')
+    var newArr = []
+    for (var i = 0; i < 9; i++) {
+      newArr = []
+      for (var j = 0; j < 9; j++) {
+        newArr.push(+arrayString[j])
+      }
+      arrayString.splice(0, 9)
+      this.result.push(newArr)
+    }
+  }
 
-  // Returns a string representing the current state of the board
-  board() {}
+  printBoard() {
+    var resultBoard = ''
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        resultBoard = resultBoard + ' ' + this.result[i][j]
+      }
+      resultBoard = resultBoard + '\n'
+    }
+    resultBoard = resultBoard + '\n'
+    return resultBoard
+  }
+
+  checkRow(number, row) {
+    for (var i = 0; i < 9; i++) {
+      if (this.result[row][i] == number) {
+        return false
+      }
+    }
+    return true
+  }
+
+  checkColumn(number, column) {
+    for (var i = 0; i < 9; i++) {
+      if (this.result[i][column] == number) {
+        return false
+      }
+    }
+    return true
+  }
+
+  solver(){    
+  }
+  
 }
 
-// The file has newlines at the end of each line,
-// so we call split to remove it (\n)
-var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
-  .toString()
-  .split("\n")[0]
-
+var board_string = "105802000090076405200400819019007306762083090000061050007600030430020501600308900"
 var game = new Sudoku(board_string)
 
-// Remember: this will just fill out what it can and not "guess"
-game.solve()
-
 console.log(game.board())
+console.log(game.printBoard());
+// console.log(game.checkColumn(1, 6));
+// console.log(game.checkColumn(6, 1));
