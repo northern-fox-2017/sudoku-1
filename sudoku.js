d9ca213305c3ce61b1589f5fec2433b0574e6f83
alangmahendra <alangmahemdra@gmail.com>
2017-11-17 18:39:30 +0700

    setor dulu boss

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,68 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.board_string = board_string
+    this.arr = []
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let arr = []
+    let index = 0
+    for (let i = 0; i < 9; i++) {
+        arr.push([])
+      for(let j=0;j < 9 ;j++){
+        arr[i].push(this.board_string[index])
+        index++
+      }
+    }
+    return arr
+  }
+
+  cekNol(){
+    let panggil = this.board()
+    for (let i = 0; i < panggil.length; i++) {
+      for (let j = 0; j < panggil.length; j++) {
+        if(panggil[i][j] == 0){
+          this.arr.push([i,j])
+      }
+    }
+  }
+  console.log(this.arr);
+}
+  cekBaris(baris,kolom,ekspektasi){
+    let panggil = this.board()
+    for (let i = 0; i < 9; i++) {
+      if(panggil[baris][i] == ekspektasi){
+        return false;
+      }
+    }
+    return true
+  }
+
+  cekKolom(baris,kolom,ekspektasi){
+    let panggil = this.board()
+    for (let i = 0; i < 9; i++) {
+      if(panggil[kolom][i] == ekspektasi){
+        return false;
+      }
+    }
+    return true
+  }
+
+  cekArea(baris,kolom,ekspektasi){
+    let row = (Math.round(this.baris/3))*3
+    let col = (Math.round(this.col/3))*3
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        array[i]
+      }
+    }
+  }
+
 }
 
 // The file has newlines at the end of each line,
@@ -21,4 +77,4 @@ var game = new Sudoku(board_string)
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
 
-console.log(game.board())
+console.log(game.cekNol())
