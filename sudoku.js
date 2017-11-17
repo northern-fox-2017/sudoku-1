db01a6ae6d38952b5dbbb6fd770b1c35e166d412
harisman <harismannugraha@gmail.com>
2017-11-17 19:19:20 +0700

    check row

--- sudoku.js
+++ sudoku.js
@@ -1,24 +1,63 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+      this.board_string = board_string;
+      this.result = [];
+  }
 
-  solve() {}
+  solve() {
+    let penampung = [];
+    for (let i = 0; i < this.result.length; i++) {
+      for (let j = 0; j < this.result[i].length; j++) {
+        if (this.result[i][j] == '0'){
+          // penampung.push(this.result[i][j])
+          for (let k = 0; k <9; k++) {
+              if (!this.checkRow(k, i) && cekkolom  && cekregio){
+                penampung.push('0')
+              }
+          }
+        }
+      }
+    }
+    return penampung;
+  }
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let counter = 0;
+    for (let i = 0; i < 9; i++) {
+      this.result.push([]);
+        for (let j = 0; j < 9; j++) {
+          this.result[i].push(this.board_string[counter])
+          counter++;
+        }
+      }
+  }
+
+  checkRow(value, row){
+    return  this.result[row].indexOf(value) !== -1;
+  }
+  checkCol(value, row){
+    return  this.result[row].indexOf(value) !== -1;
+  }
+  checkReg(value, row){
+    return  this.result[row].indexOf(value) !== -1;
+  }
 }
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
-var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
+let fs = require('fs')
+let board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[0]
 
-var game = new Sudoku(board_string)
-
+let game = new Sudoku('105802000090076405200400819019007306762083090000061050007600030430020501600308900')
+// console.log(game)
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+// game.solve()
 
 console.log(game.board())
+console.log(game.solve())
+// console.log(game.checkRow(3, 5));
