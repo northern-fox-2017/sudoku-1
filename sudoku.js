a2b1d6d2d838e5509a02c0d96249423535e11d8d
rezaalfarabi <anangreza18@gmail.com>
2017-11-18 16:26:47 +0700

    revisi release 3 + method check angka,but not solved

--- sudoku.js
+++ sudoku.js
@@ -4,83 +4,96 @@ class Sudoku {
   constructor(board_string) {
     this.number     = board_string
     this.boards     = [];
-    this.setZero       = [];
+    this.setZero    = [];
   }
 
-  solve() {
-
-  }
-
-  board() {
-    let count = 0
-    for(let i = 0 ; i < 9 ; i++) {
-      this.boards.push([]);
-        for(let j = 0 ; j < 9 ; j++) {
-          this.boards[i].push(this.number[count])
-          count++;
-        }
+    board() {
+    let newArr = this.number.split('');
+    for (var i = 0; i < newArr.length; i++) {
+      this.boards.push(newArr.splice(0, 9));
     }
-    console.log(this.boards);
+    return this.boards;
   }
 
-  getZero() { // fungsi untuk menentukan setZero baris dan kolom
-    for(let i = 0 ; i < this.boards.length ; i++) {
-      for(let j = 0 ; j < this.boards[i].length ; j++) {
-        if(this.boards[i][j] === '0') {
-          this.setZero.push([i,j]);
-        }
-      }
-    }
-    return this.setZero;
-  }
-
-  checkRow(baris, angka) {
-    for(let i = 0 ; i < 9 ; i++) {
-      if(this.boards[baris][i] == angka) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  checkKolom(kolom, angka) {
-    for(let i = 0 ; i < 9 ; i++) {
-      if(this.boards[i][kolom] == angka) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  checkArea3x3(baris, angka) {
-  let getKolom = Math.floor(baris[1] / 3) * 3;
-  let getBaris = Math.floor(baris[0] / 3) * 3;
-      for (let k = 0; k < 3; k++) {
-        for (let l = 0; l < 3; l++){
-          if (this.boards[getBaris+k][getKolom+l] == angka) {
-            return true;
+    getZero() { //untuk menentukan setzero untuk baris dan kolom
+      //cek dlu tiap angka 0 di dalam board
+      for (var i = 0; i < this.boards.length; i++) {
+        for (var j = 0; j < this.boards[i].length; j++) {
+          if (this.boards[i][j] == '0') {
+            this.setZero.push([i, j]);
           }
         }
       }
-  return false;
-  }
+      return this.setZero;
+    }
+
+    checkRow(baris, angka) {
+      //console.log(this.board_string.length)
+      for (let i = 0; i < this.boards[baris].length; i++) {
+        // debugger
+        if (this.boards[baris][i] == angka) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    checkKolom(kolom, angka) {
+      //console.log(this.boards.length)
+      for (let i = 0; i < this.boards.length; i++) {
+        // debugger
+        if (this.boards[i][kolom] == angka) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    checkArea3x3(baris, kolom, angka) {
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+
+          if (this.boards[(i + baris) - (baris % 3)][(j + kolom) - (kolom % 3)] == angka) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    checkAngka(baris, kolom, angka) {
+      if (this.checkRow(baris, angka) && this.checkKolom(kolom, angka) &&
+        this.checkArea3x3(baris, kolom, angka)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    solve() {
+
+    }
 }
 
-// The file has newlines at the end of each line,
-// so we call split to remove it (\n)
-var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
-  .toString()
-  .split("\n")[0]
+  // The file has newlines at the end of each line,
+  // so we call split to remove it (\n)
+  var fs = require('fs')
+  var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
+    .toString()
+    .split("\n")[0]
 
-var game = new Sudoku(board_string)
+  var game = new Sudoku(board_string)
 
-game.board();
-console.log(game.checkRow(2,3));
-console.log(game.checkKolom(1,3))
-// console.log(game.checkArea3x3(3,3))
+  // Remember: this will just fill out what it can and not "guess"
 
-// Remember: this will just fill out what it can and not "guess"
-// game.solve()
 
-// console.log(game.board())
+  console.log(game.board())
+  console.log(game.checkRow(0,3))
+  console.log(game.checkKolom(2,3))
+  console.log(game.checkArea3x3(0, 0, 1))
+
+  console.log(game.solve())
+  // console.log(game.getZero());
+  // console.log(game.checkAngka(1,2,4)
