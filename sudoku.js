ec5015b13ef54381c1e09b62e6f742f8a86c247c
Fajar Patappari <top.sick.red@gmail.com>
2017-11-21 10:12:21 +0700

    Performance optiimization (less loop), beautifull print board and test all puzzle available

--- sudoku.js
+++ sudoku.js
@@ -1,126 +1,111 @@
 
-"use strict"
+// "use strict"
 
 const IndexMapper = require('./indexMapper')
 
 class Sudoku {
-  constructor(board_string) {
-    this.puzzle = board_string.split('').map(Number)
-    this.mapper = new IndexMapper()
-    this.zeroMapped = this.getZeroMapped() // one time initilization
-    //this.sudoLen = 9
+  constructor() {
+    this.puzzle
+    this.zeroMapped
+    this.mapper = new IndexMapper()// one time initilization
   }
-
+  setBoard(boardString) {
+    this.puzzle = boardString.split('').map(Number)
+    this.zeroMapped = this.getZeroMapped()
+  }
   getZeroMapped() {
     return this.mapper.indexesMapped.filter(item => {
       return this.puzzle[item.i] === 0
     })
   }
-
-  solve() { 
+  solve() {
     for (let i = 0; i < this.zeroMapped.length; i++) { // loop zero occurence
       let iMap = this.zeroMapped[i]
-      let newCandidate = this.puzzle[iMap.i]
+      let newCandidate = this.puzzle[iMap.i] +1
       //console.log(this.board()) < visualisasi
       while (true) {
-        newCandidate++
-        if (newCandidate <= 9 && this.checkAll(iMap, newCandidate)) {
+        if (newCandidate <= 9 && this.isCandidateAllowed(iMap, newCandidate)) {
           this.puzzle[iMap.i] = newCandidate
           break // maju
         } else {
           if (newCandidate > 9) {
             this.puzzle[iMap.i] = 0 // sebelum mundur reset current with 0
             i -= 2 // mundur
-            break 
+            break
           }
         }
+        newCandidate++
       }
     }
   }
-
+  isCandidateAllowed(iMap, candidate) {
+    for (let i = 0; i < 9; i++) {
+      if (this.puzzle[this.mapper.rowsIndexes[iMap.iRow][i]] === candidate) {
+        return false
+      }
+      if (this.puzzle[this.mapper.columnsIndexes[iMap.iColumn][i]] === candidate) {
+        return false
+      }
+      if (this.puzzle[this.mapper.regionsIndexes[iMap.iRegion][i]] === candidate) {
+        return false
+      }
+    }
+    return true
+  }
   board() {
     var str = ''
-    let rows = this.getRows()
-    rows.forEach((row, z) => {
-      let strx = ''
-      row.forEach((a, b) => {
-        if (a == 0) {
-          strx += '\x1b[31m ' + a + ' \x1b[0m'
+    let zeroIndexes = game.zeroMapped.map(el => el.i)
+    let rows = this.mapper.rowsIndexes
+    rows.forEach((row, i) => {
+      let rowStr = ''
+      row.forEach((index, j) => {
+        if (zeroIndexes.indexOf(index) >= 0) { // index of zero
+          if (this.puzzle[index] === 0) {
+            rowStr += '\x1b[31m ' + this.puzzle[index] + ' \x1b[0m' 
+          } else {
+            rowStr += '\x1b[32m ' + this.puzzle[index] + ' \x1b[0m' 
+          }
         } else {
-          strx += '\x1b[0m ' + a + ' \x1b[0m'
+          rowStr += ' ' + this.puzzle[index] + ' '
         }
-        if ((b + 1) % 3 === 0 && b !== 8) {
-          strx += '  '
+        if ((j + 1) % 3 === 0 && j !== 8) {
+          rowStr += '  '
         }
       })
-      if ((z + 1) % 3 === 0 && z !== 8) {
-        strx += '\n'
+      if ((i + 1) % 3 === 0 && i !== 8) {
+        rowStr += '\n'
       }
-      str += strx + '\n'
+      str += rowStr + '\n'
     })
     return str + '\n - - - - - - - + - - - - - - - \n'
   }
 
-
-  getRows() {
-    return this.mapper.rowsIndexes.map(row => {
-      return row.map(i => this.puzzle[i])
-    })
-  }
-
-  checkAll(iMap, candidate) {
-    return this.checkRow(iMap.iRow, candidate) && this.checkColumn(iMap.iColumn, candidate) && this.checkRegion(iMap.iRegion, candidate)
-  }
-
-  checkRow(rowIndex, input) { //linear search
-    for (let i of this.mapper.rowsIndexes[rowIndex]) {
-      if (this.puzzle[i] === input) return false
-    }
-    return true
-  }
-  checkColumn(colIndex, input) {
-    for (let i of this.mapper.columnsIndexes[colIndex]) {
-      if (this.puzzle[i] === input) return false
-    }
-    return true
-  }
-  checkRegion(regIndex, input) {
-    for (let i of this.mapper.regionsIndexes[regIndex]) {
-      if (this.puzzle[i] === input) return false
-    }
-    return true
-  }
-
-
 }
 
-
-// let fs = require('fs')
-// let board_string = fs.readFileSync('set-01_sample.unsolved.txt')
-//   .toString()
-//   .split("\n")[1]
-
+let fs = require('fs')
+let board_stringsA = fs.readFileSync('set-01_sample.unsolved.txt')
+  .toString()
+  .split("\n")
+let board_stringsB = fs.readFileSync('set-01_sample.unsolved.txt')
+  .toString()
+  .split("\n")
+let board_strings = board_stringsA.concat(board_stringsB)
+console.time('All puzzle solved in')
+let game = new Sudoku()
+for (let i = 0; i < board_strings.length; i++) {
+  game.setBoard(board_strings[i])
+  console.time('Puzzle number ' + i + ' Solved in')
+  console.log(game.board())
+  game.solve()
+  console.log(game.board())
+  console.timeEnd('Puzzle number ' + i + ' Solved in')
+  console.log('---------------------------------------------------\n')
+}
+console.timeEnd('All puzzle solved in')
 // WORLD HARDEST SUDOKU (Arto Inkala). solved
-let board_string = '800000000003600000070090200050007000000045700000100030001000068008500010090000400'
-let game = new Sudoku(board_string)
+// let board_string = '800000000003600000070090200050007000000045700000100030001000068008500010090000400'
+// board_string = '000704005020010070000080002090006250600070008053200010400090000030060090200407000'
 
-// Remember: this will just fill out what it can and not "guess"
-console.time('Solved in')
-console.log(game.board())
-game.solve()
+console.log()
 
-
-console.log(game.board())
-console.timeEnd('Solved in')
-
-// console.log(game.checkRow(0,1))
-// console.log(game.checkColumn(3,1))
-// console.log(game.checkRegion(1,1))
-//console.log(game.zeroMapped())
-// console.log(game.getRows())
-// console.log(game.getColumns())
-// console.log(game.getRegions())
-// console.log(game.checkRow(0, 5), 'game.checkRow(0, 5)') // check input 5 dibaris ke 1
-// console.log(game.checkColumn(1, 2), 'checkColumn(1, 2)')
-// console.log(game.checkRegion(8, 3), 'checkRegion(8, 3)')
 
